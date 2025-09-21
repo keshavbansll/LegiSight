87adc98d1206b051ba6b75409830f005abaa6bdf
Keshav Bansal <officialkeshavbansal@gmail.com>
2025-09-21 11:44:54 +0530

    Server setup

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// Set EJS as the view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Serve static files (CSS, JS, images, etc.) from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Define routes for your website pages
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -19,12 +16,10 @@ app.get("/profile", (req, res) => {
   res.render("profile");
 });
 
-// Add new pricing route
 app.get("/pricing", (req, res) => {
   res.render("pricing");
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
